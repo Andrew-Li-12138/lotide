45cda3ad5a364ef21db9c58033784915700d86ed
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-05-19 23:08:44 -0400

    second to last element

---
+++ tail.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705\u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u274C\u274C Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i])
+  }
+  return newArray
+}
+
+console.log(tail([1,2,3,4,5]))
+console.log(tail([]))
+console.log(tail(["only"]))
+console.log(tail(["dog","fish","cat","shark"]))
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+assertEqual(words.length, 3); 
+assertEqual(tail(words).length, 2);
