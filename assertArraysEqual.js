7f7b8e75eb69ba4d5f75e2f095c481fbda907a33
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-05-21 20:41:48 -0400

    fixed typo

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const assertArrayEqual = function(arrayX, arrayY) {
+const assertArraysEqual = function(arrayX, arrayY) {
   if (eqArrays(arrayX, arrayY) === true) {
     console.log("\u2705\u2705 Assertion Passed: arrays are equal");
   } else {
@@ -19,7 +19,7 @@ const eqArrays = function(arrayX, arrayY) {
   return true;
 };
 
-assertArrayEqual([1, 2, 3],[1, 2, 3]);
-assertArrayEqual([1, "2" ,3], [1, 2, 3]);
-assertArrayEqual([5, 6, 7],[1, 2, 3]);
-assertArrayEqual(["1", "2", "3"],["1", "2", "3"]);
+assertArraysEqual([1, 2, 3],[1, 2, 3]);
+assertArraysEqual([1, "2" ,3], [1, 2, 3]);
+assertArraysEqual([5, 6, 7],[1, 2, 3]);
+assertArraysEqual(["1", "2", "3"],["1", "2", "3"]);
