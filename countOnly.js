beab2092af8b2edb08308178390aa088b3f6797f
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-05-23 17:08:31 -0400

    fixed SyntaxError and eslinted code

--- countOnly.js
+++ countOnly.js
@@ -11,22 +11,22 @@ const assertEqual = function(actual, expected) {
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
-//Add code to loop over all the items in the array 
-for (const item of allItems) {
-//Add an if condition to only increment our count in results if the item is found in the itemsToCount object
-if (itemsToCount[item])
-// increment the counter for each item:
-//   set a property with that string key to:
-//     the value that was already there (or zero if nothing there) with 1 added to it.
-if (results[item]) {
-  results[item] += 1;
-} else {
-  results[item] = 1;
-}
-}
-return results
-}
-
+  //Add code to loop over all the items in the array
+  for (const item of allItems) {
+    //Add an if condition to only increment our count in results if the item is found in the itemsToCount object
+    if (itemsToCount[item]) {
+      // increment the counter for each item:
+      //   set a property with that string key to:
+      //     the value that was already there (or zero if nothing there) with 1 added to it.
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
 //test code
 const firstNames = [
   "Karl",
