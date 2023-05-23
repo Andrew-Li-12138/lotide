cb7cce37f8ca857adef1cc09fae538a532827edd
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-05-23 17:00:34 -0400

    Add an if condition to only increment count if the item is found in the itemsToCount object

--- countOnly.js
+++ countOnly.js
@@ -13,11 +13,11 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 //Add code to loop over all the items in the array 
 for (const item of allItems) {
-// inside the loop, 
+//Add an if condition to only increment our count in results if the item is found in the itemsToCount object
+if (itemsToCount[item])
 // increment the counter for each item:
 //   set a property with that string key to:
 //     the value that was already there (or zero if nothing there) with 1 added to it.
-
 if (results[item]) {
   results[item] += 1;
 } else {
