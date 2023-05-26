f1a47da725e07b42e717388bee94ad1f4d9969f1
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-05-25 21:00:19 -0400

    add inspect function to make message print out more specific

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,9 @@
-const assertArraysEqual = function(arrayX, arrayY) {
-  if (eqArrays(arrayX, arrayY)) {
-    console.log("\u2705\u2705 Assertion Passed: arrays are equal");
+const assertArraysEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqArrays(actual, expected)) {
+    console.log(`\u2705\u2705 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log("\u274C\u274C Assertion Failed: arrays are not equal");
+    console.log(`\u274C\u274C Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 
 };
