d7054af135da4ae9ba7e4da7dd775b7ba78b011a
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-04 21:40:15 -0400

    export function and remove test code to separate file

--- head.js
+++ head.js
@@ -1,16 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`\u2705\u2705 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u274C\u274C Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const head = function (array) {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([11,12,13]), 14);
+module.exports = head;
