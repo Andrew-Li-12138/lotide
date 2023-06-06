e3ed90a2fa1d8c6f71618908e2babfa4ac939fd7
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-05 23:36:14 -0400

    remove test code to a separate file flattenTest.js

--- flatten.js
+++ flatten.js
@@ -1,25 +1,3 @@
-//for test code
-const assertArraysEqual = function(arrayX, arrayY) {
-  if (eqArrays(arrayX, arrayY)) {
-    console.log("\u2705\u2705 Assertion Passed: arrays are equal");
-  } else {
-    console.log("\u274C\u274C Assertion Failed: arrays are not equal");
-  }
-
-};
-
-const eqArrays = function(arrayX, arrayY) {
-  if (arrayX.length !== arrayY.length) {
-    return false;
-  }
-  for (let i = 0;  i < arrayX.length; i++) {
-    if (arrayX[i] !== arrayY[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-//...
 
 const flatten = function(arrays) {
   let flatArray = [];
@@ -35,7 +13,6 @@ const flatten = function(arrays) {
 
 module.exports = flatten;
 
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
-assertArraysEqual(flatten([23, 44, [67, 89], 56, [88, 66], 7]), [23, 44, 67, 89, 56, 88, 66, 7]);
+
 
  
