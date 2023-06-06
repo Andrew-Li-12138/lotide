322c99fd99127ea5160ad4540a5f3791efa0d950
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-06 00:05:16 -0400

    remove test code in countOnly.js to separate file countOnlyTest.js and refactored test code using Mocha & chai

--- countOnly.js
+++ countOnly.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`\u2705\u2705 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u274C\u274C Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
 
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
@@ -29,22 +21,3 @@ const countOnly = function(allItems, itemsToCount) {
 };
 
 module.exports = countOnly;
-//test code
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
---
+++ test/countOnlyTest.js
@@ -0,0 +1,33 @@
+const assert = require('chai').assert;
+const countOnly  = require('../countOnly');
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+describe("#countOnly", ( )=> {
+  it("returns 1 for result['Jason']", () => {
+    assert.strictEqual(result["Jason"], 1);
+  });
+
+  it("returns undefined for result['Karima']", () => {
+    assert.strictEqual(result["Karima"], undefined);
+  });
+
+  it("returns 2 for result['Fang']", () => {
+    assert.strictEqual(result["Fang"], 2);
+  });
+
+  it("returns undefined for result['Agouhanna']", () => {
+    assert.strictEqual(result["Agouhanna"], undefined);
+  });
+});
