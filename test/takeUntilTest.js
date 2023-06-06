3afbc488f98fa7743aa71d40dc76342a76599406
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-06-06 15:47:47 -0400

    fixed wrong name at describe bracket

--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -1,7 +1,7 @@
 const assert = require('chai').assert;
 const takeUntil = require('../takeUntil');
 
-describe("tail", ( )=> {
+describe("takeUntil", ( )=> {
   it("returns [1, 2, 5, 7, 2] for takeUntil([1, 2, 5, 7, 2, -1, 2, 4, 5], x => x < 0)", () => {
     assert.deepEqual(takeUntil([1, 2, 5, 7, 2, -1, 2, 4, 5], x => x < 0), [1, 2, 5, 7, 2]);
   });
